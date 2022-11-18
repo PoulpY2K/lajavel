281d191c528ebafa45759549d0605f9db9ebb8ab
Jérémy Laurent <contact@jeremy-laurent.com>
2022-11-18 15:47:35 +0100

    fix(main): Remove logs in main file

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -9,11 +9,6 @@ public class Main {
     public static void main(String[] args) {
         Application.start(7070, Mode.DEVELOPMENT);
 
-        Log.debug("debug");
-        Log.info("info");
-        Log.warn("warn");
-        Log.error("error");
-
         Route.register(HttpVerb.GET, "/", IndexController.class, "index");
         Route.register(HttpVerb.GET, "/get-started", GetStartedController.class, "index");
         Route.register(HttpVerb.GET, "/contributors", ContributorsController.class, "index");
